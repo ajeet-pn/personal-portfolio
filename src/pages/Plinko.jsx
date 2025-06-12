71bb7e11bc00601500c8714c5d144a70bc2c680f
ajeet-pn <ajeetpn88@gmail.com>
2025-06-12 08:30:06 +0530

    sa

--- src/pages/Plinko.jsx
+++ src/pages/Plinko.jsx
@@ -3,11 +3,10 @@
 const Plinko = () => {
   return (
     <div>
-{/* <iframe src='https://relaxed-youtiao-31ac9b.netlify.app/' className='w-full h-[100vh]' frameborder="0"
+<iframe src='https://relaxed-youtiao-31ac9b.netlify.app/' className='w-full h-[100vh]' frameborder="0"
     allowfullscreen
       sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
-    /> */}
-    coming soon
+    />
 
   </div>
   );
